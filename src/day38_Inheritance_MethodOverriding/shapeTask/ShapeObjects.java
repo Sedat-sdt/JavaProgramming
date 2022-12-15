c7ed059d01ace86559de870aa59524a2b4330f86
Sedat Maden <madensedat05@gmail.com>
2022-12-15 19:13:59 +0300

    day 39 Shape Task  is completed

--- src/day38_Inheritance_MethodOverriding/shapeTask/ShapeObjects.java
+++ src/day38_Inheritance_MethodOverriding/shapeTask/ShapeObjects.java
@@ -6,7 +6,11 @@ public class ShapeObjects {
         /*Square square=new Square(-10);
         System.out.println(square);//Invalid Side: -10.0*/
         Circle circle = new Circle(2);
-        System.out.println(circle);
+        circle.setRadius(8);
+        System.out.println(circle.getRadius());//8
+        System.out.println(circle);//Circle{radius=8.0, pi=3.14', area='200.96', perimeter='50.24'}
+        System.out.println(circle.getName());//Circle
+
 
         Square square=new Square(15);
         System.out.println(square);//Square{side=15.0, area='225.0', perimeter='60.0'}
