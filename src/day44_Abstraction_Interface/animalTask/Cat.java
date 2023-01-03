44e7ea5d9f6dff14a83053a599c112ec177abedb
Sedat Maden <madensedat05@gmail.com>
2023-01-03 13:47:28 +0300

    day 44 Abstraction animal  Task completed

---
+++ src/day44_Abstraction_Interface/animalTask/Cat.java
@@ -0,0 +1,17 @@
+package day44_Abstraction_Interface.animalTask;
+
+public final class Cat extends Animal {
+    public Cat(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName()+" eats Biryani");
+
+    }
+
+    public void Meow(){
+        System.out.println(getName()+" is meowing");
+    }
+}
